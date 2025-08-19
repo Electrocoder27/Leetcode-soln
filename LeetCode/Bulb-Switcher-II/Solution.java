6ebfbc08c5a9380cf4b27ffbcf944c0c8fedc791
Rishabh Mishra <gm1626229@gmail.com>
2025-08-20 00:17:18 +0530

    Add solution for Bulb Switcher II

---
+++ LeetCode/Bulb-Switcher-II/Solution.java
@@ -0,0 +1,6 @@
+if (presses === 0) return 1;
+if (n === 1) return 2;
+if (n === 2 && presses === 1) return 3;
+if (n === 2 || presses === 1) return 4;
+if (presses === 2) return 7;
+if (presses > 2) return 8;
