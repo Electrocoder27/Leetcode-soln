2fdeab8d39c64f7a66458382db50dc80bc888046
Rishabh Mishra <gm1626229@gmail.com>
2025-09-04 02:29:04 +0530

    Add solution for Find the Number of Ways to Place People II

--- LeetCode/Find-the-Number-of-Ways-to-Place-People-II/Solution.java
+++ LeetCode/Find-the-Number-of-Ways-to-Place-People-II/Solution.java
@@ -1,34 +1,17 @@
 class Solution {
     public int numberOfPairs(int[][] points) {
-        int n = points.length;
-        int result = 0;
-
+        int count = 0, n = points.length;
+        Arrays.sort(points, (a, b) -> a[0] == b[0] ? 
+        Integer.compare(b[1], a[1]) : Integer.compare(a[0], b[0]));
         for (int i = 0; i < n; i++) {
-            int xa = points[i][0];
-            int ya = points[i][1];
-
-            for (int j = 0; j < n; j++) {
-                if (i == j) continue;
-                int xb = points[j][0];
-                int yb = points[j][1];
-
-                // Alice must be upper-left, Bob must be lower-right
-                if (xa <= xb && ya >= yb) {
-                    boolean valid = true;
-                    for (int k = 0; k < n; k++) {
-                        if (k == i || k == j) continue;
-                        int xk = points[k][0];
-                        int yk = points[k][1];
-
-                        if (xk >= xa && xk <= xb && yk <= ya && yk >= yb) {
-                            valid = false;
-                            break;
-                        }
-                    }
-                    if (valid) result++;
+            int y = Integer.MIN_VALUE;
+            for (int j = i + 1; j < n; j++) {
+                if (points[i][1] >= points[j][1] && points[j][1] > y) {
+                    count++;
+                    y = points[j][1];
                 }
             }
         }
-        return result;
+        return count;
     }
 }
