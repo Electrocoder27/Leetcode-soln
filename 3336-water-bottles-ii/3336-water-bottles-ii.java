8e0e1407b31475b0009aac5c30c21098f8467dd5
Rishabh Mishra <gm1626229@gmail.com>
2025-10-02 11:58:56 +0530

    Time: 1 ms (30.32%), Space: 41 MB (45.81%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int count = numBottles ;
+        int empty = numBottles ;
+        while(empty>=numExchange) {
+            empty -= numExchange; 
+            count++;              
+            empty++;      
+            numExchange++; 
+        }
+        return count ;
+    }
+}
