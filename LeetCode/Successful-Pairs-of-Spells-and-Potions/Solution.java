4ac8ea68b2dbbb79307548b6bfe32b72040bdabe
Rishabh Mishra <gm1626229@gmail.com>
2025-10-09 17:45:48 +0530

    Add solution for Successful Pairs of Spells and Potions

--- LeetCode/Successful-Pairs-of-Spells-and-Potions/Solution.java
+++ LeetCode/Successful-Pairs-of-Spells-and-Potions/Solution.java
@@ -1,9 +1,29 @@
-def binarySearchForMinVal()
-    l, r = lower_bound, upper_bound
-    while l < r:
-        mid = (l + r)//2 #round down
-        if feasible(mid):
-            r = mid #check for possible smaller values that work
-        else:
-            l = mid + 1 #values smaller than or equal to mid do not work. Reduce search space to values greater than mid.
-    return l
+import java.util.*;
+
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+        int n = spells.length;
+        int m = potions.length;
+        int[] res = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            long required = (success + spells[i] - 1) / spells[i]; // ceil division
+            int idx = binarySearch(potions, required);
+            res[i] = m - idx; // remaining potions are successful
+        }
+        
+        return res;
+    }
+
+    // Find the first index in potions where potion >= required
+    private int binarySearch(int[] potions, long required) {
+        int l = 0, r = potions.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (potions[mid] < required) l = mid + 1;
+            else r = mid - 1;
+        }
+        return l;
+    }
+}
